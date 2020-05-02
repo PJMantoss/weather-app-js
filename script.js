a6ab312ba3916d2f16d842a805e8a5140dde9f4f
pjmantoss <joelptoss@gmail.com>
2020-05-02 14:49:01 +0100

    update script.js

--- script.js
+++ script.js
@@ -32,7 +32,30 @@ const displayResults = weather => {
 }
 
 const dateBuilder = d => {
-    
+    let months = [
+        "January", 
+        "February", 
+        "March", 
+        "April", 
+        "May", 
+        "June", 
+        "July", 
+        "August", 
+        "September", 
+        "October", 
+        "November", 
+        "December"
+    ];
+
+    let days = [
+        "Sunday", 
+        "Monday", 
+        "Tuesday", 
+        "Wednesday", 
+        "Thursday", 
+        "Friday", 
+        "Saturday"
+    ];
 }
 
 searchBox.addEventListener('keypress', setQuery);
