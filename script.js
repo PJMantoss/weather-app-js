8b32489c8e1446b63be097004ec23c2c1008a69c
pjmantoss <joelptoss@gmail.com>
2020-05-02 14:31:25 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const api = {
     key: "0fadf96d2336ead026424b46f3be5560",
-    baseurl: "https://api.openweathermap.org/data/2.5/"
+    base: "https://api.openweathermap.org/data/2.5/"
 }
 
 const searchBox = document.querySelector('.search-box');
 
 
 
-const setQuery = (e) => {
+const setQuery = e => {
     if(e.keyCode == 13){
-        //getResult(searchBox.value);
+        getResults(searchBox.value);
         console.log(searchBox.value);
     }
+}
+
+const getResults = query => {
+    fetch(`${api.base}weather?q=${query}&unit=metric&APPID=${api.key}`)
+      .then(weather => {
+          return weather.json();
+      }).then(displayResults);
+}
+
+const displayResults = weather => {
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
 }
 
 searchBox.addEventListener('keypress', setQuery);
